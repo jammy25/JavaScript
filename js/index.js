2202753037408c91e8a084f1c3faddaafc4034f7
jammy25 <kishan95.jammy@gmail.com>
2023-03-22 19:05:23 +0530

    upto class2 - Console Logs, Errors, Warnings

---
+++ js/index.js
@@ -0,0 +1,13 @@
+console.time('Your code took')
+console.log('Hello World');
+console.log(4+34);
+console.log(345);
+console.log(true);
+console.log([32, 4, 5, 6])
+console.log({name: 'Kishan', marks: 35})
+console.table({name: 'Harry', marks: 45})
+console.warn('This is a warning')
+// console.clear()  #This line will clear the above code and then time-timeEnd won't work
+console.timeEnd('Your code took')
+// console.assert(566 < 189, 'Error this is incorrect')
+console.error('This is an Error')
