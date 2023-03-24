cdf26486825e50e54e52853a13cbc783382f1af6
jammy25 <kishan95.jammy@gmail.com>
2023-03-25 00:00:05 +0530

    tut4

---
+++ js/tut4.js
@@ -0,0 +1,26 @@
+// Primitive data types
+
+// String
+let name = "Rumus Lupin";
+console.log("My string is " + name);
+console.log("Data type is " + (typeof name));
+
+// Number
+let marks = 65;
+console.log("Data type is " + (typeof marks));
+
+// Boolean
+let isDriver = true;
+console.log('Data type is ' + (typeof isDriver));
+
+// null
+let nullVar = null;
+console.log('Data type is ' + (typeof nullVar));
+
+// undefined
+let undef = undefined
+console.log('Data type is ' + (typeof undef))
+
+// Reference Data Type
+myarr = [2, 3, 4, 35]
+console.log('Data type is ' + (typeof myarr))
