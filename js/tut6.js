308dd54788b46fe6d11073950875dc3e944c7ca3
jammy25 <kishan95.jammy@gmail.com>
2023-03-25 23:29:56 +0530

    tut6

---
+++ js/tut6.js
@@ -0,0 +1,19 @@
+console.log("We are at tut6");
+
+// String
+
+const name = "Alfred";
+const greeting = "Good Morning";
+console.log(greeting + ' ' + name);
+
+let html;
+html = "<h1>this is HeAding</h1>" + 
+        "<p>this is a para</p>";
+
+html = html.concat('this', ' str2');
+console.log(html);
+console.log(html.length);
+console.log(html.toLowerCase())
+console.log(html.toUpperCase())
+console.log(html)
+console.log(html[1])
