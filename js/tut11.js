248800c3eb307034f30cac467c7083c7b1767f88
jammy25 <kishan95.jammy@gmail.com>
2023-03-29 23:16:19 +0530

    tut11

---
+++ js/tut11.js
@@ -0,0 +1,34 @@
+console.log(`Welcome to tut 11, let's discuss Manipulating Websites using JS Window Obj`)
+
+// let a = window;
+// console.log(a);
+/*This will show all the window methods in console*/
+
+/*window is a global variable, thus we don't have to write at every instance*/
+// window.alert('Welcome!!');
+// alert('Confirm Submission!');
+
+// a = prompt(`Please enter your name to begin`);
+// console.log(a);
+
+// a = confirm(`Are you sure you want to close the tab`)
+// console.log(a);
+/*confirm is a boolean variable*/
+
+// let a = window.document;
+// console.log(a);
+
+// a = window.innerHeight;
+// b = innerWidth;
+// console.log(a, b)
+
+// a = scrollX;
+// b = scrollY;
+// console.log(a, b)
+
+// a = location;
+// b = location.toString()
+// console.log(a, b)
+
+a = window.history;
+console.log(a)
