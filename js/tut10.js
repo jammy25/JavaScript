da16af4d0c96d53b697bf95153b2fcc9ae302f71
jammy25 <kishan95.jammy@gmail.com>
2023-03-29 10:05:42 +0530

    Function

--- js/tut10.js
+++ js/tut10.js
@@ -46,3 +46,68 @@ arr = ['fruits', 'vegetables', 'furniture']
 arr.forEach(function(element, index, array){
     console.log(element, index)
 });
+
+// Scope
+
+/*Case_1*/
+var i = 234;
+console.log(i);
+
+function ui(name){
+    var i = 9;
+    console.log(i)
+    return (`My name is ${name}`);
+}
+console.log(ui('Kishan'), i);
+
+/*Case_2*/
+var j = 234;
+console.log(i);
+
+function uj(name){
+    j = 9;
+    console.log(j)
+    return (`My name is ${name}`);
+}
+console.log(uj('Kishan'), j);
+
+/*Case_3*/
+let k = 234;
+console.log(k);
+
+function uk(name){
+    let k = 9;
+    console.log(k)
+    return (`My name is ${name}`);
+}
+console.log(uk('Kishan'), k);
+
+/*Case_4(a)*/
+{
+    var a = 234;
+    console.log(a)
+}
+console.log(a)
+
+function ua(name){
+    let a = 9;
+    console.log(a);
+    return (`My name is ${name}`)
+}
+console.log(ua('Kishan'), a)
+
+/* see var has a global level scope so once we declared a with var it will throw value everywhere, whereas let & const has block level scope and we declare them inside a func/blocj we won't receive it outside the block */
+
+/*Case_4(b)*/
+// {
+//     let b = 234;
+//     console.log(b)
+// }
+// console.log(b)
+
+// function ua(name){
+//     let b = 9;
+//     console.log(b);
+//     return (`My name is ${name}`)
+// }
+// console.log(ua('Kishan'), b)    /*--> The code will throw an error*/
