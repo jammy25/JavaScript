63afa61a4ba40f8bf88676ed628de25a54dfa036
jammy25 <kishan95.jammy@gmail.com>
2023-04-21 08:37:17 +0530

    console_logs errors and warnings

--- js/01- Console logs, errors and warnings.js
+++ js/01- Console logs, errors and warnings.js
@@ -1,11 +1,11 @@
 console.time('Your code took')
 console.log('Hello World');
-console.log(4+34);
+console.log(4 + 34);
 console.log(345);
 console.log(true);
 console.log([32, 4, 5, 6])
-console.log({name: 'Kishan', marks: 35})
-console.table({name: 'Harry', marks: 45})
+console.log({ name: 'Kishan', marks: 35 })
+console.table({ name: 'Harry', marks: 45 })
 console.warn('This is a warning')
 // console.clear()  #This line will clear the above code and then time-timeEnd won't work
 console.timeEnd('Your code took')
