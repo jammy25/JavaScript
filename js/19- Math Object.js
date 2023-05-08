0425b993c36fe09e675143ca4fae60bdaf529bf5
jammy25 <kishan95.jammy@gmail.com>
2023-05-08 07:39:40 +0530

    Math Object

--- js/19- Math Object.js
+++ js/19- Math Object.js
@@ -1,1 +1,26 @@
 console.log("Welcome to tut-19");
+let x = 3;
+let y = 6;
+let z;
+z = x + y;
+z = x - y;
+z = x * y;
+z = x / y;
+
+// Exploring the Math Object
+z = Math;
+z = Math.PI;
+z = Math.E;
+z = Math.round(5.3);
+z = Math.ceil(5.3);
+z = Math.floor(5.6);
+z = Math.floor(-5.6);
+z = Math.abs(-5);
+z = Math.sqrt(49);
+z = Math.pow(2, 3);
+z = Math.min(2, 3, 2324, 56);
+z = Math.max(2, 3, 2324, 56);
+z = Math.random();
+z = 100*Math.random();  // To get the number between 0 - 100
+z = Math.ceil(50 + (100-50)*Math.random());  // To get the number between 50 - 100
+console.log(z);
